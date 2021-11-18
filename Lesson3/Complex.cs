b49a2418283f14c36e3c1ec821fde28b08867f02
AndreyStrokan <strokan.andrey.31@gmail.com>
2021-11-18 10:54:24 +0700

    Added complex number struct

---
+++ Lesson3/Complex.cs
@@ -0,0 +1,61 @@
+﻿namespace Lesson3
+{
+    /// <summary>
+    /// Represents a complex number.
+    /// </summary>
+    internal struct Complex
+    {
+        private double realPart;
+        public double RealPart
+        {
+            get
+            {
+                return realPart;
+            }
+            set
+            {
+                realPart = value;
+            }
+        }
+
+        private double imaginaryPart;
+        public double ImaginaryPart
+        {
+            get
+            {
+                return imaginaryPart;
+            }
+            set
+            {
+                imaginaryPart = value;
+            }
+        }
+
+        /// <summary>
+        /// Constructor of a complex number.
+        /// </summary>
+        /// <param name="realPart">Real part of a complex number.</param>
+        /// <param name="imaginaryPart">Imaginary part of a complex number.</param>
+        public Complex(double realPart, double imaginaryPart)
+        {
+            this.realPart = realPart;
+            this.imaginaryPart = imaginaryPart;
+        }
+
+        /// <summary>
+        /// Overloading of the + operatop, the sum of complex numbers.
+        /// </summary>
+        /// <param name="complex1">Complex number.</param>
+        /// <param name="complex2">Complex number.</param>
+        /// <returns>The sum of complex numbers.</returns>
+        public static Complex operator +(Complex complex1, Complex complex2)
+        {
+            return new Complex(complex1.RealPart + complex2.RealPart, complex1.ImaginaryPart + complex2.ImaginaryPart);
+        }
+
+        public override string ToString()
+        {
+            return $"{realPart} {imaginaryPart}i";
+        }
+    }
+}
