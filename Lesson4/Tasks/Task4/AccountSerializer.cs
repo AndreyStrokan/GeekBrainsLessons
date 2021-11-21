6012f4f4afa015fc4ccedec520e8ba954df9a9b1
AndreyStrokan <strokan.andrey.31@gmail.com>
2021-11-22 03:54:49 +0700

    Remove unused usings

--- Lesson4/Tasks/Task4/AccountSerializer.cs
+++ Lesson4/Tasks/Task4/AccountSerializer.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Lesson4.Tasks.Task4
+﻿namespace Lesson4.Tasks.Task4
 {
     internal class AccountSerializer 
     {
